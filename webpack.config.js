b76d5efd98178031bf749989aa5a4913794328b0
Michael Larragueta <mjlarragueta@gmail.com>
2020-02-13 23:44:14 -0800

    rename file

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
         jqueryTerminal: 'jQuery.terminal'
       },
     mode: 'development',
-    entry: './src/main.js',
+    entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
