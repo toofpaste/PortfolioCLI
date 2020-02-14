3066eba385378cf514faa684e5173ef2f3803002
Michael Larragueta <mjlarragueta@gmail.com>
2020-02-14 00:16:21 -0800

    rename file

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname +'./dist'));
+app.use(express.static(__dirname +'/dist'));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './dist/index.html'));
+  res.sendFile(path.resolve(__dirname, '/dist/index.html'));
 });
 
 app.listen(port);
